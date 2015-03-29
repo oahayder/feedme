5de6270b0000f24f57464f833930aab28b4364d7
Omar Al-Hayderi <oahayder@yahoo-inc.com>
2015-03-29 14:31:08 -0700

    testing

--- feedme/manage.py
+++ feedme/manage.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    sys.path += [pwd]
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedme.settings")
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- feedme/proximitysearch/tests.py
+++ feedme/proximitysearch/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from proximitysearch.models import FoodFinderInfo
 
-# Create your tests here.
+class NearbyFoodFacilityTestCase(TestCase):
+    def setUp(self):
+        FoodFinderInfo.objects.create(name = "Testco", address="1770 Pacific", description="Imaginary food stuffs", longitude="37.794656", latitude="-122.424326")
+
+    def test_get_distances(self):
+        testco = FoodFinderInfo.objects.get(name="lion")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+        self.assertEqual(cat.speak(), 'The cat says "meow"')
