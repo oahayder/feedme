e8fc8f2f96a015b4118787d9099cb07a53ec6a96
Omar Al-Hayderi <oahayder@yahoo-inc.com>
2015-03-29 14:05:45 -0700

    testing

--- feedme/feedme/wsgi.py
+++ feedme/feedme/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+pwd = os.path.dirname(os.path.abspath(__file__))
+sys.path += [pwd]
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedme.settings")
 
 from django.core.wsgi import get_wsgi_application
